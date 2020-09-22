9cdcc4d54ed2027ebd3603d5526c2c36f471aee7
Arunkumar Krishnamoorthy <arunram.krish@gmail.com>
2020-09-22 11:25:58 +0530

    hello to cucumber

---
+++ src/main/java/com/abddhospital/bdd/hello_cucumber/App.java
@@ -0,0 +1,13 @@
+package com.abddhospital.bdd.hello_cucumber;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/abddhospital/bdd/hello_cucumber/RunCucumberTest.java
@@ -0,0 +1,10 @@
+package com.abddhospital.bdd.hello_cucumber;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(monochrome = true, plugin = {"pretty", "html:target/cucumber"})
+public class RunCucumberTest {
+}
